1ab45a127a35dc8ae1c459f58b65bf4cc0f8b0bc
Luca graves <lgrave264@west-mec.org>
2022-10-01 13:02:17 -0700

    finished new code

--- index.js
+++ index.js
@@ -1,46 +1,53 @@
 
 //function to initiate the countdown
-// function countDown(){
-//     for(var x = 50; x > -1; x--){
-//         delay(x)
-//     }
-// }
-
-// function delay(x){
-//     setTimeout(function(){ 
-//         console.log(x)
-//     }, 1000);
-// }
+//NEW CODE
 function countDown(){
-    setTimeout(function(){
-        document.getElementById("count").innerHTML = "45"
-    }, 5000);
-    setTimeout(function(){
-        document.getElementById("count").innerHTML = "40"
-    }, 10000);
-    setTimeout(function(){
-        document.getElementById("count").innerHTML = "35"
-    }, 15000);
-    setTimeout(function(){
-        document.getElementById("count").innerHTML = "30"
-    }, 20000);
-    setTimeout(function(){
-        document.getElementById("count").innerHTML = "25"
-    }, 25000);
-    setTimeout(function(){
-        document.getElementById("count").innerHTML = "20"
-    }, 30000);
-    setTimeout(function(){
-        document.getElementById("count").innerHTML = "15"
-    }, 35000);
-    setTimeout(function(){
-        document.getElementById("count").innerHTML = "10"
-    }, 40000);
-    setTimeout(function(){
-        document.getElementById("count").innerHTML = "5"
-    }, 45000);
-    setTimeout(function(){
-        document.getElementById("count").innerHTML = "0"
-        alert("BLASTOFF!!!!")
-    }, 50000);
+    for(var x = 0; x <= 60; x+=5){
+        delay(x)
+    }
 }
+
+function delay(x){
+    setTimeout(function(){
+        if(x == 60){
+            alert("Blast OFF!!!!!")
+        }
+        document.getElementById("count").innerHTML = x;
+    },x * 1000);
+}
+
+//OLD CODE
+
+// function countDown(){
+//     setTimeout(function(){
+//         document.getElementById("count").innerHTML = "45"
+//     }, 5000);
+//     setTimeout(function(){
+//         document.getElementById("count").innerHTML = "40"
+//     }, 10000);
+//     setTimeout(function(){
+//         document.getElementById("count").innerHTML = "35"
+//     }, 15000);
+//     setTimeout(function(){
+//         document.getElementById("count").innerHTML = "30"
+//     }, 20000);
+//     setTimeout(function(){
+//         document.getElementById("count").innerHTML = "25"
+//     }, 25000);
+//     setTimeout(function(){
+//         document.getElementById("count").innerHTML = "20"
+//     }, 30000);
+//     setTimeout(function(){
+//         document.getElementById("count").innerHTML = "15"
+//     }, 35000);
+//     setTimeout(function(){
+//         document.getElementById("count").innerHTML = "10"
+//     }, 40000);
+//     setTimeout(function(){
+//         document.getElementById("count").innerHTML = "5"
+//     }, 45000);
+//     setTimeout(function(){
+//         document.getElementById("count").innerHTML = "0"
+//         alert("BLASTOFF!!!!")
+//     }, 50000);
+// }
